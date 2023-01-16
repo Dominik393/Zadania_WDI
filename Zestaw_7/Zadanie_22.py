5c35820c6e7f34b650754ad6311e46a46dd16c13
Dominik393 <Dominiksmykow@wp.pl>
2023-01-16 23:32:34 +0100

    Done

---
+++ Zestaw_7/Zadanie_22.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def dodawanko(self, val):
+        p = self
+        while p.next is not None:
+            p = p.next
+        p.next = Node(val)
+
+    def printowanko(self):
+        p = self
+        while p is not None:
+            print(p.val)
+            p = p.next
+
+
+def f(header):
+    p = header
+    q = header
+    while p is not None and q is not None:
+        p = p.next
+        q = q.next.next
+        if p == q:
+            return True
+
+    return False
+
+
+lista = Node(1)
+lista.dodawanko(2)
+lista.dodawanko(3)
+lista.dodawanko(4)
+lista.dodawanko(5)
+lista.dodawanko(6)
+lista.next.next.next.next.next.next = lista.next.next.next
+
+print(f(lista))
