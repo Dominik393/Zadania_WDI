340f8ecface4ec0dc545acb7b46bc2f3f1ee6ec7
Dominik393 <Dominiksmykow@wp.pl>
2023-01-16 23:20:38 +0100

    Repaired

--- Zestaw_7/Zadanie_20.py
+++ Zestaw_7/Zadanie_20.py
@@ -19,39 +19,37 @@ def overlap(p1, p2):
     return (p1[0] <= p2[0] <= p1[1] or p1[0] <= p2[1] <= p1[1]
             or p2[0] <= p1[0] <= p2[1] or p2[0] <= p1[1] <= p2[1])
 
-def funkcja(header):
+def f(header):
     p = header
     bq = p
     q = bq.next
-    flag = False
-    while True:
+    while p is not None:
         while q is not None:
             if overlap(p.val, q.val):
                 p.val[0] = min(p.val[0], q.val[0])
                 p.val[1] = max(p.val[1], q.val[1])
                 bq.next = q.next
-                q = q.next
-                flag = True
+                bq = p
+                q = bq.next
             else:
                 bq = q
                 q = q.next
-        if not flag and p.next is None:
-            return header
-        elif p.next is not None:
-            p = p.next
-            bq = p
-            q = bq.next
-        else:
-            return header
+        p = p.next
 
 
 
 lista = Node([2,10])
+lista.dodawanko([13,15])
 lista.dodawanko([4,8])
 lista.dodawanko([9,12])
-lista.dodawanko([13,15])
-lista.printowanko()
-print()
 
-funkcja(lista)
-lista.printowanko()
+
+listunia = Node([15,19])
+listunia.dodawanko([20,21])
+listunia.dodawanko([18,20])
+listunia.dodawanko([5,6])
+listunia.dodawanko([8,12])
+listunia.dodawanko([13,14])
+
+f(listunia)
+listunia.printowanko()
